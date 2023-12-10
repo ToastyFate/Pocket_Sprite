3cfc6e1744303261ded149832975cb7a98a0e4e4
ToastyFate <142728970+ToastyFate@users.noreply.github.com>
2023-12-10 22:13:35 +0100

    Grid now correct and updated to canvas size

--- app/src/main/java/toasted/pocket_sprite/MainActivity.kt
+++ app/src/main/java/toasted/pocket_sprite/MainActivity.kt
@@ -1,16 +1,12 @@
 package toasted.pocket_sprite
 
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -18,15 +14,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.layout.ParentDataModifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import toasted.pocket_sprite.ui.theme.Pocket_SpriteTheme
 
-val gridWidth = 256
-val gridHeight = 256
-val pixelSize = 16.dp
+val gridWidth = 256f
+val gridHeight = 256f
+val pixelSize = 4f
+
 
 class MainActivity() : ComponentActivity() {
 
@@ -50,16 +45,19 @@ class MainActivity() : ComponentActivity() {
 @Composable
 fun PixelArtCanvas() {
 
-    Canvas(modifier = Modifier.fillMaxSize() ){
-        val cellsPerRow = gridWidth / pixelSize.toPx()
-        val cellsPerColumn = gridHeight / pixelSize.toPx()
-        val gridColor = Color.Black
+    Canvas(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.LightGray)
+    ){
+        val cellsPerRow = gridWidth.dp.toPx()/ pixelSize.dp.toPx()
+        val cellsPerColumn = gridHeight.dp.toPx() / pixelSize.dp.toPx()
+        val gridColor = Color.DarkGray
         for(y in 0 until cellsPerColumn.toInt()) {
             for(x in 0 until cellsPerRow.toInt()) {
                 drawRect(
                     color = gridColor,
-                    topLeft = Offset((x * pixelSize.toPx()), (y * pixelSize.toPx())),
-                    size = Size(pixelSize.toPx(), pixelSize.toPx()),
+                    topLeft = Offset(x+(x * pixelSize.dp.toPx()), y+(y * pixelSize.dp.toPx())),
+                    size = Size(pixelSize.dp.toPx(), pixelSize.dp.toPx()),
                 )
                 Log.println(Log.INFO, "PixelArtCanvas", "x: $x, y: $y")
             }
